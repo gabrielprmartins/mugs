b6f9b8d775faaf8192740ec5584a271aee539d12
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-04-07 17:30:45 -0300

    tab-nav adicionado

--- js/modules/mugs.js
+++ js/modules/mugs.js
@@ -19,25 +19,56 @@ const addthumbMugs = (mugs) => {
 };
 
 const mugsTemplate = (mugs) => {
-  mugs.forEach(({ name, description, img, price, stock }) => {
-    mugContent.innerHTML += `
-      <article class="mug-container">
-        <h2 class="subtitle">${name}</h2>
-        <p class="paragraph">${description}</p>
+  const mugsTemplate = mugs.map(({ name, description, img, price, stock }) => `
+    <article class="mug-container">
+      <h2 class="subtitle">${name}</h2>
+      <p class="paragraph">${description}</p>
 
-        <figure class="mug-image">
-          <img src="${img}" alt="Foto de uma caneca preta de porcelana">
-        </figure>
+      <figure class="mug-image">
+        <img src="${img}" alt="Foto de uma caneca preta de porcelana">
+      </figure>
 
-        <div class="buy">
-          <p>Por:</p>
-          <span class="price"><sup class="cipher">R$</sup>${price}</span>
-          <button class="sell-button">Adicionar ao carrinho</button>
-          <span class="remaining">${stock} restantes</span>
-        </div>
-      </article>
-    `;
-  });
+      <div class="buy">
+        <p>Por:</p>
+        <span class="price"><sup class="cipher">R$</sup>${price}</span>
+        <button class="sell-button">Adicionar ao carrinho</button>
+        <span class="remaining">${stock} restantes</span>
+      </div>
+    </article>
+  `).join('');
+
+  mugContent.innerHTML += mugsTemplate;
+  initTabNav();
 };
+
+const initTabNav = () => {
+  const tabControls = mugControl.querySelectorAll('li');
+  const tabContent = mugContent.querySelectorAll('article');
+  const activeClass = 'active';
+
+  const removeActiveClass = () => {
+    tabControls.forEach((control) => control.classList.remove(activeClass));
+    tabContent.forEach((content) => content.classList.remove(activeClass));
+  };
+
+  const addActiveClass = (index) => {
+    removeActiveClass();
+    tabControls[index].classList.add(activeClass);
+    tabContent[index].classList.add(activeClass);
+  };
+
+  const addTabNavEvents = (controls) => {
+    controls.forEach((control, index) => {
+      control.addEventListener('click', () => {
+        addActiveClass(index);
+      });
+    });
+  };
+    
+  if (tabControls.length && tabContent.length) {
+    addActiveClass(2);
+    addTabNavEvents(tabControls);
+  } 
+}
 
 export { addMugsIntoDom };
--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 import { addMenuEvent } from './modules/mobile-menu.js';
 import { addMugsIntoDom } from './modules/mugs.js';
-import { initTabNav } from './modules/tab-nav.js';
 
 
 addMenuEvent();
 addMugsIntoDom();
-initTabNav();
 
 
 // Fazer fetch da api das mugs
