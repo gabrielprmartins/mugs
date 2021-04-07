f4771e3f80b218e7d9904e58baa09d17c9de2ad8
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-04-07 02:46:48 -0300

    outside-click do menu

--- js/modules/mobile-menu.js
+++ js/modules/mobile-menu.js
@@ -1,13 +1,21 @@
 const menuButton = document.querySelector('.btn-menu');
 const menu = document.querySelector('.menu');
+const html = document.documentElement;
 
-const toggleMenu = () => {
-  menu.classList.toggle('active');
-  menuButton.classList.toggle('active');
+const openMenu = (event) => {
+  menu.classList.add('active');
+  menuButton.classList.add('active');
+  setTimeout(() => html.addEventListener('click', closeMenu));
+}
+
+const closeMenu = () => { 
+  menu.classList.remove('active');
+  menuButton.classList.remove('active');
+  setTimeout(() => html.removeEventListener('click', closeMenu));
 }
 
 const addMenuEvent = () => {
-  menuButton.addEventListener('click', toggleMenu);
+  menuButton.addEventListener('click', openMenu);
 }
 
 export { addMenuEvent };
