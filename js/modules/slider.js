3ec9c375a7db13f3dee894773033fe9df9cbaa13
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-04-14 00:09:50 -0300

    transição do slider

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,17 +1,23 @@
 const slideList = document.querySelectorAll('.budget-slider li');
 const activeClass = 'active';
+const dismissClass = 'dismiss';
 let index = 0;
 
 const nextSlideItem = () => {
-  slideList[index].classList.remove(activeClass);
-  index++;
-  if (index >= slideList.length) index = 0;
-  slideList[index].classList.add(activeClass);
+  slideList[index].classList.add(dismissClass);
+  setTimeout(() => {
+    slideList[index].classList.remove(dismissClass);
+    slideList[index].classList.remove(activeClass);
+    index++;
+    if (index >= slideList.length) index = 0;
+    slideList[index].classList.add(activeClass);
+  }, 1000);
+
 };
 
 const initSlider = () => {
   slideList[0].classList.add(activeClass);
-  setInterval(() => nextSlideItem(), 3000);
+  setInterval(() => nextSlideItem(), 8000);
 };
 
 export { initSlider };
